76e78a9a49b2eefb062c52a4698d5c0587a1a998
chrll17 <choirulanwar1774@gmail.com>
2024-09-16 13:49:20 +0700

    part 2 membuat htacces & mengubah App.php

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,19 @@
 <?php
-class App {
+class App
+{
     public function __construct()
     {
-        echo "class App sudah terpanggil";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            //jika ada url yg dikirimkan,ambil url nya
+            $url = rtrim($_GET['url'], '/'); //rtrim membersihkan slas diakhir
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url); //memecah string URL menjadi array berdasarkan pemisah slas
+            return $url;
+        }
     }
 }
